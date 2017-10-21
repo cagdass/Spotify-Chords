8cd78dc13ece05962dc2309ed9cd78eca1fd2aba
Çağdaş <cagdas.oztekin@ug.bilkent.edu.tr>
2017-10-21 14:13:20 +0200

    Working fine but in a crude way

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 'use-strict';
 let SpotifyWebApi = require('spotify-web-api-node');
 let config = require('./config');
-let l = require("lyric-get");
+var ugs = require('ultimate-guitar-scraper');
 
 let scopes = ['user-read-playback-state'],
     state = 'spotify_auth_state';
@@ -26,7 +26,7 @@ let router = express.Router();
 
 let port = config.port;
 
-//now we should configure the API to use bodyParser and look for 
+//now we should configure the API to use bodyParser and look for
 //JSON data in the request body
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -39,27 +39,67 @@ router.get('/', function(req, res) {
 	spotifyApi.getMyCurrentPlaybackState()
 	  .then(function(data) {
 	  	let temp  = getSongArtist(data.body);
-	  	let name = temp[0], artist = temp[1];
-		l.get(artist, name, function(err, lyrics){
-			res.type('text/html');
-			res.write('<head><title>Spotify Lyrics</title></head>');
-	    	res.write('<center>');
-			res.write('<h3>' + artist + ' - ' + name + '</h3>');
-		    if(err){
-		        console.log(err);
-		        res.write('Lyrics not found :(');
-		    }
-		    else{
-		    	lyrics = lyrics.replace(/(?:\r\n|\r|\n)/g, '<br />');
-		        res.write(lyrics);
-		    }
-	        res.write('</center>');
-	        res.end();
+	  	let name = temp[0];
+        let artist = temp[1];
 
-		});
+        let tabs = [];
+        let chords = [];
 
+        console.log('Gon search for tabs now');
+
+        ugs.search({
+            bandName: artist,
+            songName: name,
+            page: 1,
+            type: ['tabs', 'chords'],
+        }, function(error, found) {
+            if (error) {
+                console.log(error);
+            }
+            else {
+                for (let i = 0; i < found.length; i++) {
+                    console.log(i);
+                    console.log(found[i]);
+                    console.log(found[i].url);
+                    if (found[i] != undefined && found[i].url != undefined) {
+                        if (found[i].rating == undefined) {
+                            found[i].rating = 0;
+                        }
+                        if (found[i].numberRates == undefined) {
+                            found[i].numberRates = 0;
+                        }
+                        if (found[i].type == 'tab') {
+                            tabs.push(found[i]);
+                        }
+                        else if (found[i].type == 'chords') {
+                            chords.push(found[i]);
+                        }
+                    }
+                }
+
+                tabs.sort(tabSort);
+                chords.sort(tabSort);
+
+                console.log(tabs);
+                console.log(chords);
+
+                res.type('text/html');
+        		res.write('<head><title>Spotify Lyrics</title></head>');
+                res.write('<center>');
+                res.write('<h3>' + artist + ' - ' + name + '</h3>');
+                if (chords.length == 0) {
+                    res.write('Chords not found :(');
+                }
+                else {
+                    res.write('<iframe width="80%" height="900em" sandbox="allow-forms allow-scripts" name="top" src="' + chords[0].url + '">');
+                    res.write('</iframe>');
+                }
+                res.write('</center>');
+                res.end();
+            }
+        });
 	  }, function(err) {
-		
+
 		// clientId, clientSecret and refreshToken has been set on the api object previous to this call.
 		spotifyApi.refreshAccessToken()
 		  .then(function(data) {
@@ -75,7 +115,7 @@ router.get('/', function(req, res) {
 
 });
 
-router.get('/callback', function(req, res) { 
+router.get('/callback', function(req, res) {
 	let code  = req.query['code'];
 
 	spotifyApi.authorizationCodeGrant(code)
@@ -99,10 +139,35 @@ app.listen(port, function() {
     console.log(`Access localhost:${port} to see the lyrics for the currently played song` );
 });
 
+function tabSort(a, b) {
+    var key00 = a.rating;
+    var key01 = a.numberRates;
+    var key10 = b.rating;
+    var key11 = b.numberRates;
+    if (Math.abs(key00-key10) < 0.25) {
+        if (key01 > key11) {
+            return a;
+        }
+        else {
+            return b;
+        }
+    }
+    else {
+        if (key00 > key10) {
+            return a;
+        }
+        else {
+            return b;
+        }
+    }
+}
 
+function prettify(s) {
+    return s.replace(/ *\([^)]*\) */g, " ").trim();
+}
 
-function getSongArtist(body) { 
+function getSongArtist(body) {
 	let name = body.item.name;
 	let artist = body.item.artists['0'].name;
-	return [name, artist];
+	return [prettify(name), prettify(artist)];
 }
